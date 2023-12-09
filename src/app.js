465a992bf75a3a8b4853b7cff434cf6e4735a1ac
Dikean <dylan01aponte@gmail.com>
2023-12-08 22:14:48 -0600

    check role fix js

--- src/app.js
+++ src/app.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
-
 // Cargar las variables de entorno
 dotenv.config();
 
 // Importar rutas
 const usersCompanyRoutes = require('./routes/usersCompanyRoutes');
-const db = require('../src/utils/db');
+const db = require('./utils/db');
 // Otros imports de rutas...
 
 const app = express();
@@ -18,8 +17,9 @@ app.use(express.json());
 // Usar rutas
 app.use('/api/userscompany', usersCompanyRoutes);
 
-
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
+
+
