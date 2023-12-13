c3591a7a807f70a30ae780b747b255b4c7f53c13
Dikean <dylan01aponte@gmail.com>
2023-12-13 07:30:26 -0500

    New Fuction getDataByCompany

--- src/controllers/companysController.js
+++ src/controllers/companysController.js
@@ -8,3 +8,24 @@ exports.getAllCompanys = async (req, res) => {
         res.status(500).send(error.message);
     }
 };
+
+/**Obtener las ultimas 4 compañias por Adminstrador dela empresa */
+exports.getLast4CompanysById = async (req, res) => {
+    try {
+        const companys = await companysController.allDataCompanys();
+        res.json(companys);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+};
+
+/**Obtener Toda la data de una compañia By id */
+exports.getDataByCompany = async (req, res) => {
+    try {
+        const companys = await companysController.allDataCompanys();
+        res.json(companys);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+};
+
